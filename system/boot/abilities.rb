eae5f9fccd9a8bb25c5f3dba9f90e351a9a1f9bb
Anton Davydov <antondavydov.o@gmail.com>
2018-06-08 00:55:24 +0300

    Fix booting dependencies in project container

--- system/boot/abilities.rb
+++ system/boot/abilities.rb
@@ -2,11 +2,11 @@
 
 Container.finalize(:abilities) do |container|
   abilities = Kan::Application.new(
-    # board: [
+    board: [
     #   Boards::Abilities::Anonymous.new,
     #   Boards::Abilities::Regular.new,
     #   Boards::Abilities::Owner.new
-    # ]
+    ]
   )
 
   container.register(:abilities, abilities)
